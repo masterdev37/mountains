27048bc77241f4ead515799b61ac79ca6e9c41f9
baukabdi <114557808+masterdev37@users.noreply.github.com>
2024-04-24 17:30:09 +0500

    done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-	console.log('script.js работает!');
+	const swiper = new Swiper('.swiper', {
+		speed: 1200,
+		spaceBetween: 0,
+		parallax: true,
+		pagination: {
+			el: '.swiper-pagination',
+			enabled: true,
+			clickable: true,
+		}
+	});
+
+	document.querySelector('.hamburger').addEventListener('click', function() {
+		this.classList.toggle('is-active');
+		document.querySelector('main').classList.toggle('is-active');
+	});
 	
 })
